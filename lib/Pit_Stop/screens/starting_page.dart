30e84f3557520b1af4c954789696e467a101bd1a
Govindh07 <govindhv17@gmail.com>
2025-07-11 23:26:26 +0530

    Pit Stop

--- lib/Pit_Stop/screens/starting_page.dart
+++ lib/Pit_Stop/screens/starting_page.dart
@@ -1,13 +1,19 @@
 import 'package:android_studio/Pit_Stop/screens/login_page.dart';
 import 'package:android_studio/Pit_Stop/screens/signup_page.dart';
+import 'package:android_studio/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 
-void main() {
-  runApp(const MaterialApp(debugShowCheckedModeBanner: false,home: MyApp()) );}
+void main() async  {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MaterialApp(debugShowCheckedModeBanner: false,home: StartingPage()) );}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class StartingPage extends StatelessWidget {
+  const StartingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
