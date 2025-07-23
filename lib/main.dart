6310063fb1b484b27ffde136ee80ea15f6bfaee5
Govindh07 <govindhv17@gmail.com>
2025-07-23 21:02:20 +0530

    Pit Stop

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../../firebase_options.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
